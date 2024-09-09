7a26d62ac8cfed8cee52202aa5281d83c17b00b4
tsssni <dingyongyu2002@foxmail.com>
2024-09-09 20:53:32 +0800

    successfully build unikraft

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "OSLAB devenv";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
   };
 
-  outputs = { self , nixpkgs ,... }: let
+  outputs = { nixpkgs ,... }: let
     system = "aarch64-darwin";
   in {
     devShells."${system}".default = let
@@ -16,8 +16,25 @@
       packages = with pkgs; [
           clang
           lldb
+          qemu
+          coreutils-prefixed
           cmake
-          gnumake
+          (python3.withPackages (python-pkgs: with python-pkgs; [
+            pyyaml
+          ]))
+          (gnumake.overrideAttrs (oldAttrs: { configureFlags = oldAttrs.configureFlags ++ [ "--program-prefix=g" ]; }))
+          (gnused.overrideAttrs { configureFlags = [ "--program-prefix=g" ]; })
+          (gnugrep.overrideAttrs { 
+            configureFlags = [ "--program-prefix=g" ]; 
+            postInstall =
+              ''
+                echo "#! /bin/sh" > $out/bin/egrep
+                echo "exec $out/bin/grep -E \"\$@\"" >> $out/bin/egrep
+                echo "#! /bin/sh" > $out/bin/fgrep
+                echo "exec $out/bin/grep -F \"\$@\"" >> $out/bin/fgrep
+                chmod +x $out/bin/egrep $out/bin/fgrep
+              '';  
+          })
       ];
 
       shellHook = ''
