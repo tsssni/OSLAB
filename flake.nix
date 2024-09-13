9b86eadcde575db5454ddf3159a27a8e99d627a1
tsssni <dingyongyu2002@foxmail.com>
2024-09-13 09:11:45 +0800

    fix cross-cc error

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
     tsssni-nur = {
-      url = "github:tsssni/nur-packages";
+      url = "github:tsssni/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -13,41 +13,52 @@
     system = "aarch64-darwin";
   in {
     devShells."${system}".default = let
-      lib = nixpkgs.lib;
       pkgs = import nixpkgs {
         inherit system;
       };
       tsssni-lib = tsssni-nur.lib;
-      tsssni-cross = lib.mapAttrs (n: crossSystem: 
-        import nixpkgs {
-          inherit
-            system
-            crossSystem;
-        }
-      ) tsssni-lib.system.examples;
-      tsssni-pkgs = tsssni-nur.packages.${system};
+      tsssni-pkgs = tsssni-nur.pkgs {
+        localSystem = system;
+      };
+      aarch64-elf-pkgs = import nixpkgs {
+        localSystem = system;
+        crossSystem = tsssni-lib.systems.aarch64-elf;
+      };
     in pkgs.mkShell {
       packages = []
-        ++ 
-        (with pkgs; [
-          clang
-          lldb
-          qemu
-          coreutils-prefixed
-          cmake
-          (python3.withPackages (python-pkgs: with python-pkgs; [
-            pyyaml
-          ]))
-        ]) 
+        ++ (with pkgs; [
+            clang
+            lldb
+            qemu
+            coreutils-prefixed
+            cmake
+            (python3.withPackages (python-pkgs: with python-pkgs; [
+              pyyaml
+            ]))
+            (pkgsCross.aarch64-embedded.stdenv.cc)
+          ]) 
         ++ (with tsssni-pkgs; [
-          ggrep
-          gmake
-          gsed
-        ])
-        ++ (with tsssni-cross.aarch64-elf; [
-          stdenv.cc
-        ]);
+            ggrep
+            gmake
+            gsed
+          ])
+        ++ (with aarch64-elf-pkgs; [
+            stdenv.cc
+          ]);
       shellHook = ''
+        # Because it’s getting called from a Darwin stdenv, aarch64-elf-cc will pick up on
+        # Darwin-specific flags, and it will barf and die on -iframework in
+        # particular. Strip them out, so simavr can compile its test firmware.
+        cflagsFilter='s|-F[^ ]*||g;s|-iframework [^ ]*||g;s|-isystem [^ ]*||g;s|  *| |g'
+
+        # The `CoreFoundation` reference is added by `linkSystemCoreFoundationFramework` in the
+        # Apple SDK’s setup hook. Remove that because aarch64-elf-cc will fail due to file not found.
+        ldFlagsFilter='s|/nix/store/[^-]*-apple-framework-CoreFoundation[^ ]*||g'
+
+        # Make sure the global flags aren’t accidentally influencing the platform-specific flags.
+        export NIX_CFLAGS_COMPILE="$(sed "$cflagsFilter" <<< "$NIX_CFLAGS_COMPILE")"
+        export NIX_LDFLAGS="$(sed "$ldFlagsFilter;$cflagsFilter" <<< "$NIX_LDFLAGS")"
+
         exec zsh
       '';
     };
